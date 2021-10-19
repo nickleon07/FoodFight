de189a3e08a3ebd53d4fccf2f95484f8d51f83b5
nickleon07 <nick.r.leon@gmail.com>
2021-10-19 16:08:05 -0700

    oops

--- components/PriceScreen.js
+++ components/PriceScreen.js
@@ -11,7 +11,7 @@ var config = {
   method: 'get',
   url: 'https://api.yelp.com/v3/businesses/search?location=94521',
   headers: {
-    'Authorization': 'Bearer UfXAg4LUf4OZlm1yr2R5f5lr5Gu6HLPtdfWosEBTodDgU4bTR5C2TX8IIC8pakxYaeCdFc1bnAImT9YZghwAH6U43JksNKLm5m97oaBmgfW0OZggAkmt77UVBD5vYXYx'
+    'Authorization': 'Bearer '
   }
 };
 
